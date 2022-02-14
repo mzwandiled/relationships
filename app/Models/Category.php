6d3b5864c277646569053752649a0f366db6b658
Mzwandile Dladla <Mzwandile.Dladla@SIYAKHA.local>
2022-02-14 09:34:43 +0200

    deined the many to many relationship between category and product

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Models\Product;
 
 class Category extends Model
 {
     use HasFactory;
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class);
+    }
 }
